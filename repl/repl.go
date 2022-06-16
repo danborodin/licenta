b73222cc63e4a25d75504d202d2de9505763007c
Dan Borodin <danborodin231@gmil.com>
2022-06-16 17:21:35 +0300

    add evaluator p131

--- repl/repl.go
+++ repl/repl.go
@@ -1,6 +1,7 @@
 package repl
 
 import (
+	"bdlang/evaluator"
 	"bdlang/lexer"
 	"bdlang/parser"
 	"bufio"
@@ -31,12 +32,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
-
-		// for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-		// 	fmt.Printf("%+v\n", tok)
-		// }
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 
 }
