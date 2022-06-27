e79869d536b33aa1ba1da18ed3b198703a3404e6
Dan Borodin <danborodin231@gmil.com>
2022-06-27 17:19:21 +0300

    add len and print builtin functions

--- cmd/main.go
+++ cmd/main.go
@@ -10,10 +10,11 @@ func main() {
 	// if err != nil {
 	// 	panic(err)
 	// }
+	//if len(os.Args) > 2 {
 	file := os.Args[1]
-	//fmt.Println(file)
 	fileinput.Start(file)
-
-	//fmt.Printf("bdlang REPL alpha version\n")
-	//repl.Start(os.Stdin, os.Stdout)
+	// } else {
+	// 	fmt.Printf("bdlang REPL alpha version\n")
+	// 	repl.Start(os.Stdin, os.Stdout)
+	// }
 }
--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -4,6 +4,7 @@ import (
 	"bdlang/ast"
 	"bdlang/object"
 	"fmt"
+	"strconv"
 )
 
 var (
@@ -11,6 +12,47 @@ var (
 	FALSE = &object.Boolean{Value: false}
 	NULL  = &object.Null{}
 )
+
+var builtins = map[string]*object.Builtin{
+	"len": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newError("wrong number of arguments. got=%d, want=1", len(args))
+			}
+
+			switch arg := args[0].(type) {
+			case *object.String:
+				return &object.Integer{Value: int64(len(arg.Value))}
+			default:
+				return newError("argument to `len` not supported, got %s", args[0].Type())
+			}
+		},
+	},
+	"print": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) <= 0 {
+				return newError("not enoughth arguments in function 'print' call, got=%d, want 1 or more", len(args))
+			}
+
+			var str string = ""
+
+			for i := range args {
+				switch arg := args[i].(type) {
+				case *object.String:
+					str = str + " " + arg.Value
+				case *object.Integer:
+					str = str + " " + strconv.Itoa(int(arg.Value))
+				case *object.Boolean:
+					str = str + " " + strconv.FormatBool(arg.Value)
+				}
+			}
+
+			fmt.Println(str[1:])
+
+			return nil
+		},
+	},
+}
 
 func Eval(node ast.Node, env *object.Environment) object.Object {
 	switch node := node.(type) {
@@ -227,12 +269,15 @@ func evalBlockStatement(block *ast.BlockStatement, env *object.Environment) obje
 }
 
 func evalIdentifier(node *ast.Identifier, env *object.Environment) object.Object {
-	val, ok := env.Get(node.Value)
-	if !ok {
-		return newError("identifier not found: " + node.Value)
+	if val, ok := env.Get(node.Value); ok {
+		return val
 	}
 
-	return val
+	if builtin, ok := builtins[node.Value]; ok {
+		return builtin
+	}
+
+	return newError("identifier not found: " + node.Value)
 }
 
 func evalExpressions(exps []ast.Expression, env *object.Environment) []object.Object {
@@ -261,15 +306,16 @@ func evalStringInfixExpression(operator string, left, right object.Object) objec
 }
 
 func applyFunction(fn object.Object, args []object.Object) object.Object {
-	function, ok := fn.(*object.Function)
-	if !ok {
-		return newError("not a function %s", fn.Type())
+	switch fn := fn.(type) {
+	case *object.Function:
+		extendedEnv := extendFunctionEnv(fn, args)
+		evaluated := Eval(fn.Body, extendedEnv)
+		return unwrapReturnValue(evaluated)
+	case *object.Builtin:
+		return fn.Fn(args...)
+	default:
+		return newError("not a function: %s", fn.Type())
 	}
-
-	extendedEnv := extendFunctionEnv(function, args)
-	evaluated := Eval(function.Body, extendedEnv)
-
-	return unwrapReturnValue(evaluated)
 }
 
 func extendFunctionEnv(fn *object.Function, args []object.Object) *object.Environment {
